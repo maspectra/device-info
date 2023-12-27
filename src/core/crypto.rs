07625117c2564efd0615884ff3353f036b6c457f
johnnyxcy <johnny.xcy1997@outlook.com>
2023-12-27 15:43:16 +0800

    feat: shorten code

--- src/core/crypto.rs
+++ src/core/crypto.rs
@@ -1,34 +1,36 @@
 pub mod aes {
     use aes_gcm::{
         aead::{Aead, AeadCore, KeyInit, OsRng},
-        Aes256Gcm, Error, Key, Nonce,
+        Aes128Gcm, Error, Key, Nonce,
     };
+    use std::str;
 
     use base64::{engine::general_purpose, Engine as _};
 
     #[derive(serde::Serialize, serde::Deserialize)]
     struct Secret {
-        nonce: Vec<u8>,
-        cipher_text: Vec<u8>,
+        n: Vec<u8>,
+        v: Vec<u8>,
     }
 
-    pub fn generate_aes_key(key: Option<&String>) -> Key<Aes256Gcm> {
+    pub fn generate_aes_key(key: Option<&String>) -> Key<Aes128Gcm> {
         match key {
-            Some(value) => *Key::<Aes256Gcm>::from_slice(value.as_ref()),
+            Some(value) => *Key::<Aes128Gcm>::from_slice(value.as_ref()),
             None => match std::env::var("ENCRYPTION_KEY") {
-                Ok(key) => *Key::<Aes256Gcm>::from_slice(key.as_ref()),
-                Err(_) => Aes256Gcm::generate_key(OsRng),
+                Ok(key) => *Key::<Aes128Gcm>::from_slice(key.as_ref()),
+                Err(_) => Aes128Gcm::generate_key(&mut OsRng),
             },
         }
     }
 
-    pub fn encrypt(key: &Key<Aes256Gcm>, value: &str) -> Result<String, Error> {
-        let cipher = Aes256Gcm::new(key);
-        let nonce = Aes256Gcm::generate_nonce(&mut OsRng); // 96-bits; unique per message
-        let cipher_text = cipher.encrypt(&nonce, value.as_ref())?;
+    pub fn encrypt(key: &Key<Aes128Gcm>, value: &str) -> Result<String, Error> {
+        let cipher = Aes128Gcm::new(key);
+        let nonce = Aes128Gcm::generate_nonce(&mut OsRng); // 96-bits; unique per message
+        let encrypted = cipher.encrypt(&nonce, value.as_ref())?;
+
         let secret = Secret {
-            nonce: nonce.clone().to_vec(),
-            cipher_text: cipher_text.clone(),
+            n: nonce.clone().to_vec(),
+            v: encrypted.clone(),
         };
         let encoded =
             general_purpose::STANDARD_NO_PAD.encode(serde_json::to_string(&secret).unwrap());
@@ -36,16 +38,16 @@ pub mod aes {
         Ok(encoded)
     }
 
-    pub fn decrypt(key: &Key<Aes256Gcm>, encoded: &str) -> Result<String, Error> {
-        let cipher = Aes256Gcm::new(key);
+    pub fn decrypt(key: &Key<Aes128Gcm>, encoded: &str) -> Result<String, Error> {
+        let cipher = Aes128Gcm::new(key);
         let decoded_buffer = general_purpose::STANDARD_NO_PAD.decode(encoded).unwrap();
         let decoded_str = match std::str::from_utf8(&decoded_buffer) {
             Ok(v) => v,
             Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
         };
         let decoded_secret = serde_json::from_str::<Secret>(decoded_str).unwrap();
-        let decoded_nonce = Nonce::from_slice(&decoded_secret.nonce);
-        let buffer = cipher.decrypt(decoded_nonce, decoded_secret.cipher_text.as_ref())?;
+        let decoded_nonce = Nonce::from_slice(&decoded_secret.n);
+        let buffer = cipher.decrypt(decoded_nonce, decoded_secret.v.as_ref())?;
         let s = match std::str::from_utf8(&buffer) {
             Ok(v) => v,
             Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
@@ -61,11 +63,11 @@ mod tests {
         builder::{IMainBuilder, MainDeviceInfoBuilder},
         crypto::aes,
     };
-    use aes_gcm::{aead::OsRng, Aes256Gcm, KeyInit};
+    use aes_gcm::{aead::OsRng, Aes128Gcm, KeyInit};
 
     #[test]
     fn test_encrypt() {
-        let key = Aes256Gcm::generate_key(OsRng);
+        let key = Aes128Gcm::generate_key(&mut OsRng);
         let encrypted = aes::encrypt(&key, "Hello World");
 
         assert!(encrypted.is_ok());
@@ -73,7 +75,7 @@ mod tests {
 
     #[test]
     fn test_decrypt() {
-        let key = Aes256Gcm::generate_key(OsRng);
+        let key = Aes128Gcm::generate_key(&mut OsRng);
         let encrypted = aes::encrypt(&key, "Hello World");
         let decrypted = aes::decrypt(&key, &encrypted.unwrap());
 
@@ -83,11 +85,11 @@ mod tests {
 
     #[test]
     fn test_device_info() {
-        let key = Aes256Gcm::generate_key(OsRng);
+        let key = Aes128Gcm::generate_key(&mut OsRng);
         let mut builder = MainDeviceInfoBuilder::new();
         builder.add_user_name().add_os_distro().add_platform_name();
 
-        let encrypted = aes::encrypt(&key, builder.to_string().as_str());
+        let encrypted = aes::encrypt(&key, serde_json::to_string(&builder).unwrap().as_str());
         let decrypted = aes::decrypt(&key, &encrypted.unwrap());
 
         assert!(decrypted.is_ok());
